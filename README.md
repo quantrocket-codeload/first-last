379427a0ef10805e2bcd3de90f4c5e5d0f1fa59a
Mach8 <mach8@quantrocket.com>
2019-02-19 15:03:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# first-last
+
+Intraday momentum strategy that buys (sells) the S&P 500 when the first half hour return and penultimate half hour return are both positive (negative). Uses VIX filter to restrict strategy to high volatility regimes. Uses 30-minute data from Interactive Brokers. Runs in Moonshot.
+
+## Clone in QuantRocket
+
+CLI:
+
+```shell
+quantrocket codeload clone 'first-last'
+```
+
+Python:
+
+```python
+from quantrocket.codeload import clone
+clone("first-last")
+```
+
+## Browse in GitHub
+
+Start here: [first_last/Introduction.ipynb](first_last/Introduction.ipynb)
+
+***
+
+Find more code in QuantRocket's [Codeload Library](https://www.quantrocket.com/code/)
---
+++ first_last/first_last.py
@@ -0,0 +1,99 @@
+# Copyright 2019 QuantRocket LLC - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from moonshot import Moonshot
+from moonshot.commission import PerShareCommission
+from quantrocket.history import get_historical_prices
+
+class USStockCommission(PerShareCommission):
+    IB_COMMISSION_PER_SHARE = 0.005
+    
+class FirstHalfHourPredictsLastHalfHour(Moonshot):
+    """
+    Intraday strategy that buys (sells) if the market is up (down) during the first
+    and penultimate half-hour.
+    """
+
+    CODE = 'first-last'
+    DB = 'spy-30min'
+    DB_TIMES = ['10:00:00', '15:00:00', '15:30:00']
+    DB_FIELDS = ['Open','Close']
+    COMMISSION_CLASS = USStockCommission   
+    SLIPPAGE_BPS = 0.5
+    MIN_VIX = None
+    BENCHMARK = 756733
+    BENCHMARK_TIME = "15:30:00"
+
+    def prices_to_signals(self, prices):
+
+        closes = prices.loc["Close"]
+        opens = prices.loc["Open"]
+        
+        # Calculate first half-hour returns (including overnight return)
+        prior_closes = closes.xs('15:30:00', level="Time").shift()
+        ten_oclock_prices = opens.xs('10:00:00', level="Time")
+        first_half_hour_returns = (ten_oclock_prices - prior_closes) / prior_closes
+        
+        # Calculate penultimate half-hour returns
+        fifteen_oclock_prices = opens.xs('15:00:00', level="Time")
+        fifteen_thirty_prices = closes.xs('15:00:00', level="Time")
+        penultimate_half_hour_returns = (fifteen_thirty_prices - fifteen_oclock_prices) / fifteen_oclock_prices
+
+        # long when both are positive, short when both are negative
+        long_signals = (first_half_hour_returns > 0) & (penultimate_half_hour_returns > 0)
+        short_signals = (first_half_hour_returns < 0) & (penultimate_half_hour_returns < 0)
+        
+        # Combine long and short signals
+        signals = long_signals.astype(int).where(long_signals, -short_signals.astype(int))
+           
+        # filter by VIX
+        if self.MIN_VIX:
+            # Query VIX at 15:30 NY time (= close of 14:00:00 bar because VIX is Chicago time)
+            vix = get_historical_prices("vix-30min", 
+                                        fields="Close", 
+                                        start_date=signals.index.min(), 
+                                        end_date=signals.index.max(), 
+                                        times="14:00:00")
+            # extract VIX and squeeze single-column DataFrame to Series
+            vix = vix.loc["Close"].xs("14:00:00", level="Time").squeeze()
+            # reshape VIX like signals
+            vix = signals.apply(lambda x: vix)
+            signals = signals.where(vix >= self.MIN_VIX, 0)
+
+        return signals
+
+    def signals_to_target_weights(self, signals, prices):
+
+        # only one instrument, so allocate all capital
+        target_weights = signals.copy()
+        return target_weights
+
+    def target_weights_to_positions(self, target_weights, prices):
+
+        # We enter on the same day as the signals/target_weights
+        positions = target_weights.copy()
+        return positions
+
+    def positions_to_gross_returns(self, positions, prices):
+
+        opens = prices.loc["Open"]
+        closes = prices.loc["Close"]
+
+        # Our signal came at 15:30 and we enter at 15:30
+        entry_prices = opens.xs("15:30:00", level="Time")
+        session_closes = closes.xs("15:30:00", level="Time")
+
+        pct_changes = (session_closes - entry_prices) / entry_prices
+        gross_returns = pct_changes * positions
+        return gross_returns
